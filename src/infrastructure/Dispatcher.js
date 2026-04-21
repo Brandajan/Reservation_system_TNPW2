d241c12758356eba326e9cdc374d1b3ed88d24f6
janbranda <honzabranda@seznam.cz>
2026-04-21 18:01:10 +0200

    ready na práci studenta B

---
+++ src/infrastructure/Dispatcher.js
@@ -0,0 +1,38 @@
+//IR02 centrální zpracování akcí - Jan Branda
+import { getState, setState } from "./State.js";
+
+export const dispatch = (action) => {
+    const currentState = getState();
+    
+    switch (action.type) {
+        //akce pro entitu RentalUnit
+        case "PUBLISH_UNIT" : {
+            //zde později volání business logiky pro kontrolu
+
+            //zatím simulace změny pro tonyho
+            const newUnits = currentState.rentalUnits.map(unit => unit.id ===action.payload.unitID ? {
+                ...unit, status: "ACTIVE"
+            } : unit);
+            setState({rentalUnits: newUnits});
+            break;
+        }
+        //akce pro IR03 - Async (tony)
+        case "API_CALL_START" : {
+            //načítací kolečko
+            setState({ui: {...currentState.ui, isLoading: true, error: null}});
+            break;
+        }
+        case "API_CALL_SUCCESS": {
+            //data stažena - tony v payloadu posílá stažená ubytka
+            setState({
+                rentalUnits: action.payload.units,
+                ui: {...currentState.ui, isLoading: false}
+            });
+            break;
+        }
+
+        //zde přidávat další akce
+        default:
+            console.warn(`Neznámá akce: ${action.type}`);
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import {state} from "./infrastructure/State.js";
+import {dispatch} from "./infrastructure/Dispatcher.js";
+import {initRouter} from "./infrastructure/Router.js";
+
+initRouter();
